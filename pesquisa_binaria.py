3f27502ecf0ff4c9302b0a55e1553736341eb899
Adelino Sousa Manuel <adilsonsousaas82@gmail.com>
2025-09-18 09:14:54 +0100

    feat: update algoritmo

--- pesquisa_binaria.py
+++ pesquisa_binaria.py
@@ -1,6 +1,11 @@
+#   Algoritmo para pesquisa binária
+#   Requer que a lista esteja ordenada
 def pesquisa_binaria(lista, item):
+    # Posicao inicial
     baixo = 0
+    # Posicao final
     alto = len(lista)-1
+    # Enquanto a posicao inicial for menor ou igual a posicao final
     while baixo <= alto:
         meio = (baixo+alto)/2
         chute=lista[meio]
@@ -11,6 +16,7 @@ def pesquisa_binaria(lista, item):
         else:
             baixo = meio +1
     return None 
+# Testando o algoritmo
 minha_lista = [1, 3, 5, 7, 9]
 print pesquisa_binaria(minha_lista, 3)
 print pesquisa_binaria(minha_lista, -1)
